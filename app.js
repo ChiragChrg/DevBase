1fcf30dcc071c9a8e66af646534829bcc117645f
ChiragChrg <58076548+ChiragChrg@users.noreply.github.com>
2021-02-06 02:47:56 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -10,27 +10,34 @@ function closeNav() {
 // Dark mode Toggle
 const btn = document.querySelector("#toggle");
 const theme = document.querySelector("#theme");
+const mTheme = document.querySelector("#mTheme");
 const currentTheme = localStorage.getItem("mytheme");
 
 if (currentTheme == "dark") {
   theme.href = "/css/dark.css";
-  btn.innerHTML = `<img src="/assets/sun.svg" alt="Light" width="42px" />`;
+  mTheme.href = "/css/mDark.css";
+  btn.innerHTML = `<img id="Dark-icon" src="/assets/sun.svg" alt="Light" width="42px" />`;
   // console.log("dark-ganja");
 } else {
   theme.href = "/css/light.css";
-  btn.innerHTML = `<img src="/assets/moon.svg" alt="Light" width="42px" />`;
+  mTheme.href = "/css/mLight.css";
+  btn.innerHTML = `<img id="Dark-icon" src="/assets/moon.svg" alt="Light" width="42px" />`;
   // console.log("light-ganja");
 }
 
 btn.addEventListener("click", function () {
   if (theme.getAttribute("href") == "/css/light.css") {
     theme.href = "/css/dark.css";
+    mTheme.href = "/css/mDark.css";
     mytheme = "dark";
-    btn.innerHTML = `<img src="/assets/sun.svg" alt="Light" width="42px" />`;
+    btn.innerHTML = `<img id="Dark-icon" src="/assets/sun.svg" alt="Light" width="42px" />`;
+    // closeNav();
   } else {
     theme.href = "/css/light.css";
+    mTheme.href = "/css/mLight.css";
     mytheme = "light";
-    btn.innerHTML = `<img src="/assets/moon.svg" alt="Dark" width="42px" />`;
+    btn.innerHTML = `<img id="Dark-icon" src="/assets/moon.svg" alt="Dark" width="42px" />`;
+    // closeNav();
   }
 
   localStorage.setItem("mytheme", mytheme);
