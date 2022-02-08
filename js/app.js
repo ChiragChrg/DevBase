2d1bb028db064e24b74c566429e058f12763d780
ChiragChrg <chiruchirag2001@gmail.com>
2022-02-08 20:37:28 +0530

    About page Darkmode button Fix

--- js/app.js
+++ js/app.js
@@ -9,39 +9,41 @@ function closeNav() {
   document.querySelector("body").style.overflow = "auto";
 }
 
+// console.log(window.matchMedia("(display-mode: standalone)"));
+
 // Dark mode Toggle
-const Toggle = document.querySelector("#toggle");
 const ToggleHolder = document.querySelector(".toggleHolder");
-
+const Toggle = document.querySelector("#toggle");
 const Theme = document.querySelector("#theme");
-// const Root = document.documentElement.style;
-// const Root = getComputedStyle(Theme, ":root");
-// var Root = document.styleSheets[0].cssRules[0].style[0];
-// console.log(Root.getPropertyValue("--background"));
-// console.log(Root);
-
 var MobileTheme = document.querySelector("#mTheme");
+const AboutThm = document.querySelector("#abtTheme");
 const CurrentTheme = localStorage.getItem("Theme");
 var SetTheme;
 
 if (CurrentTheme == "dark") {
   Theme.href = "/css/dark.css";
   MobileTheme.href = "/css/Mobile/mDark.css";
+  AboutThm.href = "/css/aboutD.css";
   Toggle.className = "fad fa-lightbulb-on fa-2x";
   ToggleHolder.title = "Turn On Light Mode";
   // console.log("dark-ganja");
 } else {
   Theme.href = "/css/light.css";
   MobileTheme.href = "/css/Mobile/mLight.css";
+  AboutThm.href = "/css/aboutL.css";
   Toggle.className = "fad fa-lightbulb fa-2x";
   ToggleHolder.title = "Turn On Dark Mode";
   // console.log("light-ganja");
 }
 
 function changeTheme() {
-  if (Theme.getAttribute("href") == "/css/light.css") {
+  if (
+    Theme.getAttribute("href") == "/css/light.css" ||
+    AboutThm.getAttribute("href") == "/css/aboutL.css"
+  ) {
     Theme.href = "/css/dark.css";
     MobileTheme.href = "/css/Mobile/mDark.css";
+    AboutThm.href = "/css/aboutD.css";
     SetTheme = "dark";
     Toggle.className = "on fad fa-lightbulb-on fa-2x";
     ToggleHolder.title = "Turn On Light Mode";
@@ -49,6 +51,7 @@ function changeTheme() {
   } else {
     Theme.href = "/css/light.css";
     MobileTheme.href = "/css/Mobile/mLight.css";
+    AboutThm.href = "/css/aboutL.css";
     SetTheme = "light";
     Toggle.className = "off fad fa-lightbulb fa-2x";
     ToggleHolder.title = "Turn On Dark Mode";
